ebff4d35766aa7efb099284e22c354140c83c047
kayozuzarte <kayozuzarte@gmail.com>
2026-07-30 16:02:43 -0300

    Update sitemap URL to new domain

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,7 +1,7 @@
 export default function sitemap() {
   return [
     {
-      url: 'https://SEU-DOMINIO.com/',
+      url: 'https://kayozuzarte.com.br/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
